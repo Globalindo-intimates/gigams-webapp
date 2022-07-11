3cfa68bee311040cd15380475f15d105335f979b
YudhiFitri <fitriwah84@gmail.com>
2022-07-11 17:07:30 +0700

    commit at 11072022 1707

--- app/Modules/DataMaster/Views/bagian_view.php
+++ app/Modules/DataMaster/Views/bagian_view.php
@@ -1,4 +1,4 @@
-<?= $this->extend('layouts/page_layout'); ?>
+<?= $this->extend('layouts\page_layout'); ?>
 <?= $this->section('content'); ?>
     <!-- DataTables -->
     <link rel="stylesheet" href="<?= base_url('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css'); ?>">
@@ -7,26 +7,13 @@
 
         <!-- SweetAlert2 -->
         <link rel="stylesheet" href="<?= base_url('plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css'); ?>">
-    <style>
-    td.details-control {
-        background: url(<?= base_url('images/details_open.png'); ?>) no-repeat center center;
-        cursor: pointer;
-        width: 30px;
-        transition: .5s;
-    }        
-    tr.shown td.details-control {
-        background: url(<?= base_url('images/details_close.png'); ?>) no-repeat center center;
-        width: 30px;
-        transition: .5s;
-    }    
-    </style>
 
     <div class="container h-100">
         <div class="row h-100 justify-content-center align-items-center">
-            <div class="col-md-10">
+            <div class="col-md-6">
                 <div class="card card-info shadow">
                     <div class="card-header">
-                        <h3 class="card-title">Data Master Menu</h3>
+                        <h3 class="card-title">Data Master Bagian</h3>
                         <div class="card-tools">
                             <button type="button" class="btn btn-tool" data-card-widget="collapse">
                                 <i class="fas fa-minus"></i>
@@ -34,21 +21,19 @@
                         </div>
                     </div>
                     <div class="card-body">
-                        <table class="table table-striped table-bordered" id="tableApp">
+                        <table id="tableBagian" class="table table-bordered table-striped">
                             <thead>
                                 <tr>
-                                    <th></th>
                                     <th>ID</th>
-                                    <th>Menu</th>
-                                    <th>Icon</th>
+                                    <th>Bagian</th>
+                                    <th></th>
                                 </tr>
                             </thead>
                             <tfoot>
                                 <tr>
-                                    <th></th>
                                     <th>ID</th>
-                                    <th>Menu</th>
-                                    <th>Icon</th>
+                                    <th>Bagian</th>
+                                    <th></th>
                                 </tr>
                             </tfoot>
                         </table>
@@ -58,42 +43,38 @@
         </div>
     </div>
 
-    <div class="modal fade" id="modalApp" role="dialog">
+    <div class="modal fade" id="modalBagian" role="dialog">
         <div class="modal-dialog modal-sm">
-            <div class="modal-content bg-Info">
+            <div class="modal-content bg-success">
                 <div class="modal-header">
-                    <h4 class="modal-title">Data Module App</h4>
+                    <h4 class="modal-title">Data Bagian</h4>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times</span>
                     </button>
                     <div class="ribbon-wrapper">
-                        <div class="ribbon bg-warning" id="theRibbon1">
+                        <div class="ribbon bg-warning" id="theRibbon">
                         </div>
                     </div>
                 </div>
-                <form id="formApp" name="formApp">
-                    <input type="hidden" id="id">
+                <form id="formBagian" name="formBagian">
+                    <input type="hidden" id="id_bagian">
                     <div class="modal-body">
-                        <div class="form-group">
-                            <label for="module">Module:</label>
-                            <select id="module" name="module" class="form-control"></select>
-                        </div>
-                        <div class="form-group">
-                            <label for="route">Route:</label>
-                            <input type="text" id="route" name="route" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="caption">Caption:</label>
-                            <input type="text" id="caption" name="caption" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="alias">Alias:</label>
-                            <input type="text" id="alias" name="alias" class="form-control">
+                        <div class="row">
+                            <!-- <div class="col-6"> -->
+                                <div class="form-group row">
+                                    <div class="col-sm-6 text-right p-1">
+                                        <label class="col-form-label">Bagian:</label>
+                                    </div>
+                                    <div class="col-sm-6 p-1">
+                                        <input type="text" id="bagian" name="bagian" class="form-control form-control-sm">
+                                    </div>
+                                </div>
+                            <!-- </div> -->
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="submit" id="btnSaveApp" class="btn btn-info"><i class="fa fa-upload"></i>&nbsp;Save</button>
-                        <button type="button" id="btnCancelApp" class="btn btn-danger" data-dismiss="modal" aria-label="Close"><i class="fa fa-close"></i>&nbsp;Cancel</button>
+                        <button type="submit" id="btnSaveBagian" class="btn btn-info"><i class="fa fa-upload"></i>&nbsp;Save</button>
+                        <button type="button" id="btnCancelBagian" class="btn btn-danger" data-dismiss="modal" aria-label="Close"><i class="fa fa-close"></i>&nbsp;Cancel</button>
                     </div>
                 </form>
             </div>
@@ -127,158 +108,127 @@
             position: 'center',
             showConfirmButton: true,
         });
-        var childTable;
-        var rowData;    
         $.fn.dataTable.ext.errMode = 'none';
 
-        const tableApp = $('#tableApp').on('error.dt', function(e, settings,techNote, message){
+        const tableBagian = $('#tableBagian').on('error.dt', function(e, settings,techNote, message){
             console.log(message);
             window.open('<?= base_url("auth/getAuth"); ?>', '_self')
         }).DataTable({
-            ajax:{
-                type: 'GET',
-                url: '<?= base_url("MasterData/daftarMenu"); ?>',
-                dataType: 'json'
-            },
-            columns: [
+            // "dom": 'rf<"toolbar">tip',
+            // dom: 'flrB<"toolbar">tip',
+            "dom": 'lrfBtip',
+            "buttons": [
                 {
-                    className: 'details-control',
-                    data: null,
-                    defaultContent: ''
-                },
-                {data: 'id'},
-                {data: 'module'},
-                {data: 'icon'},
-            ],
-            columnDefs: [
-                {
-                    targets: [1],
-                    visible: false
-                },
-            ]
-        });
-
-        $('#tableApp tbody').on('click', 'td.details-control', function(){
-            let tr = $(this).closest('tr');
-            let row = tableApp.row(tr);
-            rowData = row.data();
-
-            console.log('rowData: ', rowData);
-
-            if(row.child.isShown()){
-                row.child.hide();
-                tr.removeClass('shown');
-                $('#cT' + rowData.clientID).DataTable().destroy();
-            }else{
-                row.child(format(rowData)).show();
-                let id = rowData.id;
-
-                childTable = $(`#cT${id}`). DataTable({
-                    "dom": 'lrfBtip',
-                    "buttons": [
-                        {
-                            text: 'Add New App',
-                            action: function(e, dt, node, config){
-                                $('#theRibbon1').text('Add New');
-                                clearAppControls();
-                                $('#module').append($('<option>', {
-                                    value: rowData.id,
-                                    text: rowData.module
-                                }));
-                                $('#route').val($('#module').text() + "/");
-                                // $('#module').val(rowData.id);
-                                // $('#module').text(rowData.module);
-
-                                $('#modalApp').modal('show');                        
-                            }
-                        }
-                    ],
-                    ajax: {
-                        type: 'GET',
-                        url: '<?= base_url("MasterData/aplikasi/detailApp"); ?>/' + rowData.id,
-                        dataType: 'json'
-                    },
-                    columns: [
-                        {data: 'id'},
-                        {data: 'route'},
-                        {data: 'caption'},
-                        {data: 'alias'},
-                        {data: null},
-                    ],
-                    columnDefs: [
-                        {
-                            targets: [0],
-                            visible: false
-                        },
-                        {
-                            targets: [4],
-                            render: function(data, type, row, meta){
-                                return `<button class="btn btn-info btn-sm btnEditApp"><i class="fas fa-edit"></i> Edit</button>
-                                        <button class="btn btn-danger btn-sm btnDeleteApp"><i class="fas fa-Trash"></i> Hapus</button>`;
-                            }
-                        }                        
-                    ]
-                });
-                tr.addClass('shown');
-            }
-        });
-
-        $('#tableApp tbody').on('click', 'button.btnEditMenu', function(){
-            let selectedRow = tableApp.row($(this).parents('tr')).data();
-
-            $('#menu').val(selectedRow.module);
-            $('#alias').val(selectedRow.alias);
-            $('#icon').val(selectedRow.icon);
-            $('#theRibbon').text('Edit');
-            $('#modalMenu').modal('show');
-        })
-
-        function format(rD){
-            var childTable = `<table class="table table-striped table-bordered no-wrap" id="cT${rD.id}" width="100%">
-                                <thead>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Route</th>
-                                        <th>Caption</th>
-                                        <th>Alias</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                             </table>`;
-            return $(childTable).toArray();
-        }
-
-        function clearAppControls(){
-            $('#id').val('');
-            $('#module').empty();
-            $('#route').val('');
-            $('#caption').val('');
-            $('#alias').val('');
-        }
-
-        $('form[id="formApp"]').validate({
-            rules: {
-                route: {
-                    'required': true
-                },
-                caption: {
-                    'required': true
-                },
-                alias: {
-                    'required': true
+                    text: 'Add New',
+                    action: function(e, dt, node, config){
+                        $('#theRibbon').text('Add New');
+                        $('#id_bagian').val('');
+                        $('#bagian').val('');
+                        $('#modalBagian').modal('show');                        
+                    }
                 }
+            ],
+            "paging": true,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "responsive": true, 
+            "ajax": {
+                url: '<?= base_url("MasterData/daftarBagian") ;?>',
+                type: 'GET',
+                dataType: 'json',
+            },
+            "columns": [
+                {'data': 'id'},
+                {'data': 'bagian'},
+                {'data': null},
+            ],
+            "columnDefs": [
+                {
+                    "targets": [0],
+                    "visible": false
+                },
+                {
+                    "targets": [2],
+                    "render": function(data, type, row, meta) {
+							return '<button class="btn btn-success btn-sm mx-1 btnEditBagian"><i class="fas fa-edit"></i>Edit</button>' +
+								'<button class="btn btn-danger btn-sm mx-1 btnDeleteBagian"><i class="fas fa-trash"></i>Delete</button>';  
+                    }                  
+                }
+            ],
+
+        });
+
+        $('div.toolbar').html('<button id="btnAddNewBagian" class="btn btn-warning btn-sm"><i class="fas fa-plus"></i>Add New</button>');
+
+        $('#tableBagian tbody').on('click', 'button.btnEditBagian', function() {
+            let selectedRow = tableBagian.row($(this).parents('tr')).data();
+            console.log('selectedRow: ', selectedRow);
+            $('#id_bagian').val(selectedRow.id);
+            $('#bagian').val(selectedRow.bagian);
+            $('#theRibbon').text('Edit');
+            $('#modalBagian').modal('show');
+		});
+
+        $('#tableBagian tbody').on('click', 'button.btnDeleteBagian', function() {
+            let selectedRow = tableBagian.row($(this).parents('tr')).data();
+
+            const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: 'btn btn-success',
+                cancelButton: 'btn btn-danger'
+            },
+                buttonsStyling: false
+            })
+
+            swalWithBootstrapButtons.fire({
+                title: 'Yakin akan menghapus?',
+                text: "Data yang sudah dihapus tidak bisa dikembalikan lagi!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Ya, hapus saja!',
+                cancelButtonText: 'Tidak, batalkan!',
+                reverseButtons: true
+            }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    type: 'DELETE',
+                    url: '<?= base_url("MasterData/bagian/delete"); ?>/' + selectedRow.id,
+                    dataType: 'json'
+                }).done(function(dtReturn){
+                    if(dtReturn.status == 200){
+                        swalWithBootstrapButtons.fire(
+                            'sudah dihapus!',
+                            'Data sudah dihapus.',
+                            'success'
+                        );
+                        loadTable();
+                    }
+                })
+            } else if (
+                /* Read more about handling dismissals below */
+                result.dismiss === Swal.DismissReason.cancel
+            ){
+                swalWithBootstrapButtons.fire(
+                    'Dibatalkan',
+                    'Data tidak jadi dihapus :)',
+                    'error'
+                )
+            }
+            })            
+		});        
+        
+        $('form[id="formBagian"]').validate({
+            rules: {
+                bagian: {
+                    'required': true
+                },
             },
             errorClass: 'error fail-alert',
             validClass: 'valid success-alert',
             messages: {
-                route: {
-                    required: 'Route harus diisi!'
-                },
-                caption: {
-                    required: 'Caption harus diisi!'
-                },
-                alias: {
-                    required: 'Alias harus diisi!'
+                bagian: {
+                    required: 'Bagian harus diisi!'
                 },
             },
             submitHandler: function(form, e) {
@@ -286,8 +236,8 @@
 
                 let data = $(form).serialize();
 
-                const url = $('#theRibbon1').text() == 'Add New' ? '<?= base_url("MasterData/aplikasi/create"); ?>' : '<?= base_url("MasterData/aplikasi/update"); ?>/' + $('#id').val();
-                const method = $('#theRibbon1').text() == 'Add New' ? 'POST' : 'PUT';
+                const url = $('#theRibbon').text() == 'Add New' ? '<?= base_url("MasterData/bagian/create"); ?>' : '<?= base_url("MasterData/bagian/update"); ?>/' + $('#id_bagian').val();
+                const method = $('#theRibbon').text() == 'Add New' ? 'POST' : 'PUT';
 
                 $.ajax({
                     type: method,
@@ -304,28 +254,20 @@
                     }else{
                         Toast.fire({
                             icon: 'success',
-                            title: 'Data App berhasil disimpan.',
+                            title: 'Data Bagian berhasil disimpan.',
                             didClose: () => {
                                 $('#bagian').focus();
-                                $('#theRibbon').text() == 'Edit' ? $('#modalApp').modal('hide') : clearAppControls();
+                                $('#theRibbon').text() == 'Edit' ? $('#modalBagian').modal('hide') : $('#bagian').val('');
                                 loadTable();
                             }
                         });                            
                     }
                 })
-            }            
+            }
         });
-
-        function loadData(){
-            tableApp.ajax.reload();
+        
+        function loadTable(){
+            tableBagian.ajax.reload();
         }
-
-        function clearAppControls(){
-            $('#module').val('');
-            $('#route').val('');
-            $('#caption').val('');
-            $('#alias').val('');
-        }
-
     </script>
 <?= $this->endSection(); ?>
